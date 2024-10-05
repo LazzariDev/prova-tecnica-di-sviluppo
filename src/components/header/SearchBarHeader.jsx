90bfc2abc50ef2e09a81404e4a039b01e2bc7a46
LazzariDev <lazzaridev@gmail.com>
2024-10-05 10:46:07 +0200

    SearchBarHeader

--- src/components/header/SearchBarHeader.jsx
+++ src/components/header/SearchBarHeader.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SearchBarHeader = () => {
-  return (
-    <div id='search-bar-header' className='w-full h-[52px] lg:h-24 bg-red-500'>
+    const [menuClicked, setMenuClicked] = useState(false);
+    const [isInputDisabled, setIsInputDisable] = useState(true);
+    const [searchClicked, setSearchClicked] = useState(false);
 
+    const handleToggleMenu = () => {
+        setMenuClicked(prevState => !prevState);
+
+    }
+
+    const handleToggleSearch = () => {
+        setSearchClicked(prevState => !prevState)
+        setIsInputDisable(prevState => !prevState)
+    }
+
+  return (
+    <div id='search-bar-header' className='w-full h-[52px] lg:h-24 sticky flex items-center justify-between'>
+        <button className='h-[52px] w-[52px] lg:w-24 lg:h-24 text-red-600 border border-black' onClick={handleToggleMenu}>
+            {menuClicked ? <i className="fa-solid fa-bars"></i> : <i className="fa-solid fa-xmark"></i>}
+        </button>
+        <input className='w-full h-[52px] lg:h-24 flex border border-black focus:outline-none disabled:opacity-100 disabled:bg-white' disabled={!isInputDisabled} type="text" name="" id="" />
+        <button className='h-[52px] w-[52px] lg:w-24 lg:h-24 text-red-600 border border-black' onClick={handleToggleSearch}>
+            {searchClicked ? <i className="fa-solid fa-magnifying-glass"></i> : <i className="fa-solid fa-xmark"></i>}
+        </button>
     </div>
   )
 }
