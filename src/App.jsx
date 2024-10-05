3464cba84a0c2ffcaea281e0a8f2b2d74b2eabbf
LazzariDev <lazzaridev@gmail.com>
2024-10-05 09:03:17 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const App = () => {
+
+  return (
+    <>
+        <h1 className="text-3xl font-bold underline">
+        Prova tecnica di sviluppo
+        </h1>
+    </>
+  )
+}
+
+export default App
